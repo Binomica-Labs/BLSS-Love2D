bbf608ca167c04f143dfb55fe4d2d34d0c94960c
Andrew Minnich <aminnich3@gmail.com>
2019-07-08 21:09:54 -0400

    visualize dna

--- conf.lua
+++ conf.lua
@@ -1,3 +1,4 @@
 function love.conf(t)
 	t.identity = "BLSS-Workspace"
+	t.window.resizable = true
 end
--- main.lua
+++ main.lua
@@ -1,11 +1,19 @@
 local inspect = require 'lib.inspect'
 local util = require 'util'
 
+local nucleotideColors = {
+	A = {220/255, 255/255, 255/255},
+	T = {255/255, 255/255, 0},
+	C = {230/255, 0, 0},
+	G = {0, 0, 0},
+	X = {255/255, 0, 255/255}
+}
+
 local function loadDna(filePath)
 	local lines = {}
 	-- read lines from a file, ignoring comments
 	for line in love.filesystem.lines(filePath) do
-		if line:sub(1, 2) ~= '>' then
+		if line:sub(1, 1) ~= '>' then
 			table.insert(lines, line)
 		end
 	end
@@ -53,9 +61,22 @@ local function abstractDna(dnaString, abstractionLevel)
 	return table.concat(dnaChunks)
 end
 
+local function getDnaImage(dnaString)
+	local imageHeight = math.max(200, util.round(#dnaString / 200))
+	local imageData = love.image.newImageData(200, imageHeight)
+	imageData:mapPixel(function(x, y)
+		local nucleotideIndex = x + y * 200
+		local nucleotide = dnaString:sub(nucleotideIndex + 1, nucleotideIndex + 1)
+		local color = nucleotideColors[nucleotide]
+		return unpack(color)
+	end)
+	return love.graphics.newImage(imageData)
+end
+
 local dnaString = loadDna 'test-data/deino.fna'
-local abstractedDna = abstractDna(dnaString, 100)
+local dnaImage = getDnaImage(dnaString)
 
 function love.draw()
 	love.graphics.print('Memory usage: ' .. math.floor(collectgarbage 'count') .. ' kb')
+	love.graphics.draw(dnaImage, 300, 0)
 end
